2822da016f1e0d2c7e1b39396f54adc04ff4a951
ArgentumWalker <svidchen-ko@mail.ru>
2017-09-10 13:58:53 +0300

    [homework0000] Refactor Parser

--- homework0000/src/main/scala/ru/spbau/svidchenko/homework0000/calculator/ExpressionParser.scala
+++ homework0000/src/main/scala/ru/spbau/svidchenko/homework0000/calculator/ExpressionParser.scala
@@ -8,7 +8,6 @@ import ru.spbau.svidchenko.homework0000.calculator.expression.{Expression, Value
 /**
   * @author ArgentumWalker
   */
-@throws(classOf[exceptions.EmptyExpressionException])
 object ExpressionParser {
   def parse(expression: String): Expression = {
     val stringExpression: String = expression.replaceAll(" ","")
@@ -16,138 +15,151 @@ object ExpressionParser {
       throw new exceptions.EmptyExpressionException
     }
 
-    var argumentsStack: util.Stack[util.ArrayList[Expression]] = new util.Stack[util.ArrayList[Expression]]
-    var namesStack: util.Stack[String] = new util.Stack[String]
-    var startPos: Int = 0
-    var state: Int = 0
+    var parseState : ParseState = new ParseState
     //0 - start parsing expression or parsing closing bracket sequence,
-    //1  - parsing expression name
-    //2 - parsing value decimal part, 3 - parsing value after point part
+    //1  - parsing expression name, 2 - parsing closing bracket sequence,
+    //3 - parsing value decimal part, 4 - parsing value after point part
 
     for (pos <- 0 until stringExpression.length) {
-      //Handle 0 state
-      if (state.equals(0)) {
-        stringExpression(pos) match {
-          case ch if '0' to '9' contains ch => state = 2
-          case '-' => state = 2
-          case ')' =>
-            if (argumentsStack.empty()) {
-              throw new exceptions.SyntaxErrorException
-            }
-            val arguments: util.ArrayList[Expression] = argumentsStack.pop()
-            val name: String = namesStack.pop()
-            val closedExpression: Expression = ExpressionFactory.operation(name, arguments)
-            if (!argumentsStack.empty()) {
-              argumentsStack.peek().add(closedExpression)
-            } else {
-              if (!pos.equals(stringExpression.length - 1)) {
-                throw new exceptions.SyntaxErrorException
-              }
-              return closedExpression
-            }
-          case ',' =>
-            if (argumentsStack.empty()) {
-              throw new exceptions.SyntaxErrorException
-            }
-          case ch if List('(', '.') contains ch => throw new exceptions.EmptyExpressionException
-          case _ => state = 1
-        }
-        startPos = pos
-      }
-      state match {
-        case 0 =>
-          //Do nothing; Handler above
-        case 1 =>
-          stringExpression(pos) match {
-            //Start argument matching
-            case '(' =>
-              state = 0
-              namesStack.push(stringExpression.substring(startPos, pos))
-              argumentsStack.push(new util.ArrayList[Expression]())
-            //End argument matching of parent operation
-            case ')' =>
-              if (argumentsStack.empty()) {
-                throw new exceptions.SyntaxErrorException
-              }
-              argumentsStack.peek().add(ExpressionFactory.operation(
-                stringExpression.substring(startPos, pos),
-                new util.ArrayList[Expression]()
-              ))
-
-              val arguments: util.ArrayList[Expression] = argumentsStack.pop()
-              val name: String = namesStack.pop()
-              val closedExpression: Expression = ExpressionFactory.operation(name, arguments)
-              if (!argumentsStack.empty()) {
-                argumentsStack.peek().add(closedExpression)
-              } else {
-                if (!pos.equals(stringExpression.length - 1)) {
-                  throw new exceptions.SyntaxErrorException
-                }
-                return closedExpression
-              }
-            //Start next argument
-            case ',' =>
-              if (argumentsStack.empty()) {
-                throw new exceptions.SyntaxErrorException
-              }
-              argumentsStack.peek().add(ExpressionFactory.operation(
-                stringExpression.substring(startPos, pos),
-                new util.ArrayList[Expression]()
-              ))
-              state = 0
-            case '.' =>
-              throw new exceptions.SyntaxErrorException
-            case _ => //Do nothing
-          }
-        case st if List(2, 3) contains st =>
-          stringExpression(pos) match {
-            case '.' =>
-              if (state.equals(3)) {
-                throw new SyntaxErrorException
-              }
-              state = 3
-            //Next argument of parent function
-            case ',' =>
-              if (namesStack.empty()) {
-                throw new exceptions.SyntaxErrorException
-              }
-              state = 0
-              val value : Value = ExpressionFactory.value(stringExpression.substring(startPos, pos).toDouble)
-              argumentsStack.peek().add(value)
-            //End argument matching of parent operation
-            case ')' =>
-              if (namesStack.empty()) {
-                throw new exceptions.SyntaxErrorException
-              }
-              state = 0
-              val value : Value = ExpressionFactory.value(stringExpression.substring(startPos, pos).toDouble)
-              argumentsStack.peek().add(value)
-              val arguments: util.ArrayList[Expression] = argumentsStack.pop()
-              val name: String = namesStack.pop()
-              val closedExpression: Expression = ExpressionFactory.operation(name, arguments)
-              if (!argumentsStack.empty()) {
-                argumentsStack.peek().add(closedExpression)
-              } else {
-                if (!pos.equals(stringExpression.length - 1)) {
-                  throw new exceptions.SyntaxErrorException
-                }
-                return closedExpression
-              }
-            case _ => //Do nothing
-          }
+      parseState.pos = pos
+      parseState.state match {
+        case 0 => startOfExpressionState(parseState, stringExpression)
+        case 1 => expressionNameState(parseState, stringExpression)
+        case 2 => closingBracketSequenceState(parseState, stringExpression)
+        case x if List(3, 4) contains x => valueState(parseState, stringExpression)
       }
     }
     //If expression still not constructed
-    if (!namesStack.empty() || state.equals(0)) {
+    if (!parseState.namesStack.empty() || parseState.state.equals(0)) {
       throw new exceptions.SyntaxErrorException
     }
-    if (state.equals(1)) {
+    if (parseState.finished) {
+      return parseState.finalExpression
+    }
+    if (parseState.state.equals(1)) {
       ExpressionFactory.operation(
-        stringExpression.substring(startPos),
+        stringExpression.substring(parseState.startPos),
         new util.ArrayList[Expression]()
       )
     } else {
-      ExpressionFactory.value(stringExpression.substring(startPos).toDouble)
+      ExpressionFactory.value(stringExpression.substring(parseState.startPos).toDouble)
     }
+  }
+
+  private def startOfExpressionState(parseState: ParseState, expression: String): Unit = {
+    parseState.startPos = parseState.pos
+    expression(parseState.pos) match {
+      case ch if '0' to '9' contains ch =>
+        parseState.state = 3
+        valueState(parseState, expression)
+      case '-' =>
+        parseState.state = 3
+        valueState(parseState, expression)
+      case ch if List('(', '.', ')', ',') contains ch => throw new exceptions.EmptyExpressionException
+      case _ =>
+        parseState.state = 1
+        expressionNameState(parseState, expression)
+    }
+  }
+
+  private def closingBracketSequenceState(parseState: ParseState, expression: String): Unit = {
+    expression(parseState.pos) match  {
+      case ')' =>
+        buildTopExpression(parseState, expression)
+      case ',' =>
+        parseState.state = 0
+      case _ =>
+        throw new exceptions.SyntaxErrorException
+    }
+  }
+
+  private def expressionNameState(parseState: ParseState, expression: String): Unit = {
+    expression(parseState.pos) match {
+      //Start argument matching
+      case '(' =>
+        parseState.state = 0
+        parseState.namesStack.push(expression.substring(parseState.startPos, parseState.pos))
+        parseState.argumentsStack.push(new util.ArrayList[Expression]())
+      //End argument matching of parent operation
+      case ')' =>
+        if (parseState.argumentsStack.empty()) {
+          throw new exceptions.SyntaxErrorException
+        }
+        parseState.argumentsStack.peek().add(ExpressionFactory.operation(
+          expression.substring(parseState.startPos, parseState.pos),
+          new util.ArrayList[Expression]()
+        ))
+        buildTopExpression(parseState, expression)
+      //Start next argument
+      case ',' =>
+        if (parseState.argumentsStack.empty()) {
+          throw new exceptions.SyntaxErrorException
+        }
+        parseState.argumentsStack.peek().add(ExpressionFactory.operation(
+          expression.substring(parseState.startPos, parseState.pos),
+          new util.ArrayList[Expression]()
+        ))
+        parseState.state = 0
+      case '.' =>
+        throw new exceptions.SyntaxErrorException
+      case _ => //Do nothing
+    }
+  }
+
+  private def valueState(parseState: ParseState, expression: String): Unit = {
+    expression(parseState.pos) match {
+      case '.' =>
+        if (parseState.state.equals(4)) {
+          throw new SyntaxErrorException
+        }
+        parseState.state = 4
+      //Next argument of parent function
+      case ',' =>
+        if (parseState.namesStack.empty()) {
+          throw new exceptions.SyntaxErrorException
+        }
+        parseState.state = 0
+        val value : Value = ExpressionFactory.value(expression.substring(parseState.startPos, parseState.pos).toDouble)
+        parseState.argumentsStack.peek().add(value)
+      //End argument matching of parent operation
+      case ')' =>
+        if (parseState.namesStack.empty()) {
+          throw new exceptions.SyntaxErrorException
+        }
+        parseState.state = 2
+        parseState.argumentsStack.peek().add(
+          ExpressionFactory.value(expression.substring(parseState.startPos, parseState.pos).toDouble)
+        )
+        buildTopExpression(parseState, expression)
+      case _ => //Do nothing
+    }
+  }
+
+  private def buildTopExpression(parseState: ParseState, expression: String): Unit = {
+    if (parseState.argumentsStack.empty()) {
+      throw new exceptions.SyntaxErrorException
+    }
+    val arguments: util.ArrayList[Expression] = parseState.argumentsStack.pop()
+    val name: String = parseState.namesStack.pop()
+    val closedExpression: Expression = ExpressionFactory.operation(name, arguments)
+    if (!parseState.argumentsStack.empty()) {
+      parseState.argumentsStack.peek().add(closedExpression)
+    } else {
+      if (!parseState.pos.equals(expression.length - 1)) {
+        throw new exceptions.SyntaxErrorException
+      }
+      parseState.finished = true
+      parseState.finalExpression = closedExpression
+    }
+  }
+
+  private class ParseState {
+    var pos: Int = 0
+    var state: Int = 0
+    var startPos: Int = 0
+    var argumentsStack: util.Stack[util.ArrayList[Expression]] = new util.Stack[util.ArrayList[Expression]]
+    var namesStack: util.Stack[String] = new util.Stack[String]
+    var finished: Boolean = false
+    var finalExpression: Expression = _
   }
 }
